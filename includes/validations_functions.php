a81dd56ff0ae787ed8f788124d1ed1fb03c587f8
okama12 <158049375+okama12@users.noreply.github.com>
2024-02-02 21:00:45 +0300

    Update validations_functions.php

--- includes/validations_functions.php
+++ includes/validations_functions.php
@@ -16,13 +16,13 @@ function sendemail_verify($fullname, $email, $verify_token){
         $mail->isSMTP();                                            
         $mail->Host       = 'smtp.gmail.com';                     
         $mail->SMTPAuth   = true;                                   
-        $mail->Username   = 'socialme835@gmail.com';                     
-        $mail->Password   = 'oqjl hmbp uvbj elle';                               
+        $mail->Username   = '';                     
+        $mail->Password   = '';                               
         $mail->SMTPSecure = 'tls';            
         $mail->Port       = 587;                                    
 
         //Recipients
-        $mail->setFrom('socialme835@gmail.com', $fullname);
+        $mail->setFrom('', $fullname);
         $mail->addAddress($email);     //Add a recipient
 
         //Content
